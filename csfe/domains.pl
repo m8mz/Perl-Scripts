5418779f0c7a5816b0fe791b431dfaccd3a1fd50
root <root@DESKTOP-SC7TBDV.localdomain>
2019-02-28 21:14:59 -0700

    .

---
+++ csfe/domains.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use Getopt::Long qw(GetOptions);
+use Data::Dumper;
+use File::Basename qw(dirname);
+use Cwd qw(abs_path);
+use lib dirname( abs_path $0 ) . '/lib';
+
+use CSFE;
+
+if (csfe_check_all()) {
+	my $username;
+	GetOptions('username|u=s' => \$username) or die "Usage: $0 [--username|-u] USER\n";
+	if (!$username) {
+		die "Usage: $0 [--username|-u] USER\n";
+	}
+	my $res = csfe_post_request({
+
+	});
+	if ($res) {
+		my @lines = split /\n/, $res;
+	} else {
+		die "Post request failed!\n";
+	}
+} else {
+	die "Failed CSFE check_all().\n";
+}
+
